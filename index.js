77f508c208eda6a8fb611b2c82b8e4f67b5e5876
zigmons <rafael_sousa87@hotmail.com>
2022-10-18 20:47:18 -0300

    inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+// let pratoDoDia = "Lasanha";
+// let precoPratoDoDia = 24.90;
+// let frete = 13.89;
+// let cliente = "Rafael";
+
+// let total = precoPratoDoDia + frete;
+
+// console.log(`O total do pedido do ${cliente} para o pedido de ${pratoDoDia} foi de: ${total}`)
+
+console.log(10 >= 100);
+console.log(0 < 1000);
+console.log(888 == 889);
+console.log(20 <= 20);
+console.log(77 == "77");
+console.log(77 === "77");
