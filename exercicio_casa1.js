41ac29710a1fe705aee2720ad0c1e11a36fda72a
zigmons <rafael_sousa87@hotmail.com>
2022-10-19 15:24:24 -0300

    correção

--- exercicio_casa1.js
+++ exercicio_casa1.js
@@ -1,3 +1,7 @@
+let produto1 = "Curology Moisturizer"
+let produto2 = "Curology Lip gloss"
+let produto3 = "Curology cleanser"
+
 let moisturizer = 60.00
 let lip_gloss = 80.00
 let cleanser = 24.00
@@ -6,7 +10,7 @@ let qnt_moist = 2
 let qnt_lipgloss = 1
 let qnt_clean = 3
 
-let frete_total = 24.00
+let frete_total = parseFloat(24.00).toFixed(2)
 let tax = 8.25
 
 let moisturizer_float = moisturizer*qnt_moist
@@ -18,22 +22,46 @@ let total = parseFloat(subtotal) + frete_total + tax
 
 
 const carrinho = [
-    moisturizer_float, 
-    lip_gloss_float, 
-    cleanser_float,
-    subtotal,
-    frete_total,
-    tax,
-    total,
+    {
+        produto1,
+        moisturizer_float,
+        qnt_moist
+    },
+    {
+        produto2,
+        lip_gloss_float,
+        qnt_lipgloss
+    },
+    {
+        produto3,
+        cleanser_float,
+        qnt_clean
+    },
+    {
+        subtotal
+    },
+    {
+        frete_total
+    },
+    {
+        tax
+    },
+    {
+        total
+    },
+
 ];
 
-console.log(`Curology Moisturizer x${qnt_moist} $ ${parseFloat(carrinho[0]).toFixed(2)}`);
-console.log(`Curology Lip gloss x${qnt_lipgloss} $ ${parseFloat(carrinho[1]).toFixed(2)}`);
-console.log(`Curology cleanser x${qnt_clean} $ ${parseFloat(carrinho[2]).toFixed(2)}`);
-console.log(`Subtotal $ ${parseFloat(carrinho[3]).toFixed(2)}`);
-console.log(`Frete $ ${parseFloat(carrinho[4]).toFixed(2)}`);
-console.log(`Tax $ ${parseFloat(carrinho[5]).toFixed(2)}`);
-console.log(`Total $ ${parseFloat(carrinho[6]).toFixed(2)}`);
+
+
+console.log(`${carrinho[0].produto1} x${carrinho[0].qnt_moist} $ ${parseFloat(carrinho[0].moisturizer_float).toFixed(2)}`);
+console.log(`${carrinho[1].produto2} x${carrinho[1].qnt_lipgloss} $ ${parseFloat(carrinho[1].lip_gloss_float).toFixed(2)}`);
+console.log(`${carrinho[2].produto3} x${carrinho[2].qnt_clean} $ ${parseFloat(carrinho[2].cleanser_float).toFixed(2)}`);
+console.log(`Subtotal $ ${carrinho[3].subtotal}`)
+console.log(`Frete $ ${carrinho[4].frete_total}`)
+console.log(`Tax $ ${carrinho[5].tax}`)
+console.log(`Total $ ${carrinho[6].total}`)
+
 
 // function ver(){
 //     let nome = document.getElementById("nome")
